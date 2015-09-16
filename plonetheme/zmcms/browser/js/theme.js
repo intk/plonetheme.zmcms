7ad07d9613ca7b89ba4b4909787808ad1ba8df10
André Gonçalves <andreslb1@gmail.com>
2015-09-16 15:48:09 +0200

    Request all tabs after DOM is ready via ajax. Load widgets after tab ready.

--- plonetheme/zmcms/browser/js/theme.js
+++ plonetheme/zmcms/browser/js/theme.js
@@ -4,41 +4,40 @@
 --------------------------------------------------------------------------------- */
 
 
-$(document).ready(function() {
-    
-    // Disable inputs in private view
-    $("div.template-edit input, div.template-edit select:not(.formTabs), div.template-edit textarea, div.template-edit button").prop("disabled", true);
+var ajaxLoadTabs = function(fieldset_id) {
+    $("body.template-edit select.formTabs").prop("disabled", true);
 
-    $("body.template-edit select.formTabs").change(function() {
-        var select_element = $(this);
-        var option_selected = $(this).find("option:selected");
-        var fieldsetlegend_id = option_selected.attr("id");
-        var fieldset_id = fieldsetlegend_id.replace("fieldsetlegend-", "");
-        
+    if ($("body").hasClass("template-edit")) {
+        $(document).trigger('readyAgain', [{fieldset_id: "#fieldset-default"}]);
         if (fieldset_id != "default") {
             var query = "?fieldset=" + fieldset_id + "&ajax_load=true";
             var link = window.location.href;
             url = link + query
 
-            var original_fieldset = $("fieldset#fieldset-"+fieldset_id);
-            original_fieldset.show();
-
             $.ajax({
                 url: url,
                 success: function(data) {
-                    var fieldset = $(data).find("fieldset#fieldset-"+fieldset_id);
-                    var original_fieldset = $("fieldset#fieldset-"+fieldset_id);
-                    fieldset.find('legend').remove();
-                    original_fieldset.html(fieldset.html());
-                    $(document).trigger('readyAgain');
-                    original_fieldset.show();
+                    var fieldsets = $(data).find("fieldset");
+                    fieldsets.each(function() {
+                        var _id = $(this).attr("id");
+                        if (_id != 'fieldset-default') {
+                            var fieldset = $(this);
+                            var original_fieldset = $("fieldset#"+_id);
+                            fieldset.find('legend').remove();
+                            original_fieldset.html(fieldset.html());
+                            var real_fieldset_id = "#"+_id;
+                            $(document).trigger('readyAgain', [{fieldset_id: real_fieldset_id}]);
+                        } 
+                    });
+                    $("body.template-edit select.formTabs").prop("disabled", false);
                 }
             });
         }
+    }
+}
 
-
-    });
+$(document).ready(function() {
+    // Disable inputs in private view
+    $("div.template-edit input, div.template-edit select:not(.formTabs), div.template-edit textarea, div.template-edit button").prop("disabled", true);
+    ajaxLoadTabs("all");
 });
-
-
-
