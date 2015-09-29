944901f0fb642a8efaed51d81701905331fc265e
André Gonçalves <andreslb1@gmail.com>
2015-09-29 18:18:49 +0200

    Add support for outgoing loan

--- plonetheme/zmcms/browser/js/theme.js
+++ plonetheme/zmcms/browser/js/theme.js
@@ -32,7 +32,7 @@ var show_ajax_error = function(textStatus, errorThrown) {
 };
 
 var in_allowed_portaltypes = function() {
-    if ($("body").hasClass("portaltype-object") || $("body").hasClass("portaltype-book") || $("body").hasClass('portaltype-personorinstitution') || $("body").hasClass('portaltype-exhibition') || $("body").hasClass('portaltype-audiovisual') || $("body").hasClass('portaltype-treatment')) {
+    if ($("body").hasClass("portaltype-object") || $("body").hasClass("portaltype-book") || $("body").hasClass('portaltype-personorinstitution') || $("body").hasClass('portaltype-exhibition') || $("body").hasClass('portaltype-audiovisual') || $("body").hasClass('portaltype-treatment') || $("body").hasClass('portaltype-outgoingloan')) {
         return true;
     }
     return false;
@@ -166,6 +166,12 @@ var ajaxLoadTabs = function(fieldset_id) {
                                     var original_fieldset = $("fieldset#"+_id);
                                     original_fieldset.html(fieldset.html());
                                 } 
+                            } else if ($("body").hasClass('portaltype-outgoingloan')) {
+                                if (_id != 'fieldset-default' && _id != 'fieldset-loan_request') {
+                                    var fieldset = $(this);
+                                    var original_fieldset = $("fieldset#"+_id);
+                                    original_fieldset.html(fieldset.html());
+                                } 
                             } else {
                                 if (_id != 'fieldset-default' && _id != 'fieldset-identification') {
                                     var fieldset = $(this);
@@ -224,6 +230,9 @@ $(document).ready(function() {
                     } else if ($("body").hasClass("portaltype-treatment")) {
                         init_widgets($("fieldset#fieldset-treatment_details"));
                         createRelatedItemsLink("fieldset#fieldset-treatment_details", 3000);
+                    } else if ($("body").hasClass("portaltype-outgoingloan")) {
+                        init_widgets($("fieldset#fieldset-loan_request"));
+                        createRelatedItemsLink("fieldset#fieldset-loan_request", 3000);
                     }
                 }
             } else {
@@ -238,6 +247,8 @@ $(document).ready(function() {
                         createRelatedItemsLink("fieldset#fieldset-exhibitions_details", 3000);
                     } else if ($("body").hasClass("portaltype-treatment")) {
                         createRelatedItemsLink("fieldset#fieldset-treatment_details", 3000);
+                    } else if ($("body").hasClass("portaltype-outgoingloan")) {
+                        createRelatedItemsLink("fieldset#fieldset-loan_request", 3000);
                     }
                 }
             }
@@ -286,6 +297,15 @@ $(document).ready(function() {
             data_id = $(this).val();
             element = $("fieldset#"+data_id);
             if (!element.hasClass('widgets-init') && data_id != "fieldset-treatment_details") {
+                init_datagrid(element);
+                init_widgets(element);
+                element.addClass('widgets-init');
+                createRelatedItemsLink("fieldset#"+data_id, 300);
+            }
+        } else if ($("body").hasClass("portaltype-outgoingloan")) {
+            data_id = $(this).val();
+            element = $("fieldset#"+data_id);
+            if (!element.hasClass('widgets-init') && data_id != "fieldset-loan_request") {
                 init_datagrid(element);
                 init_widgets(element);
                 element.addClass('widgets-init');
