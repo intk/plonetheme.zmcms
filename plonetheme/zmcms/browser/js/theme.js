8482194aa503f473f71e18371df1b6f20004d976
André Gonçalves <andreslb1@gmail.com>
2015-11-16 14:52:38 +0100

    Sortable table view

--- plonetheme/zmcms/browser/js/theme.js
+++ plonetheme/zmcms/browser/js/theme.js
@@ -526,8 +526,41 @@ var fix_textareas_height = function(elem) {
     });
 }
 
+
+function replaceCollectionTable(overrides) {
+    var defaults = {
+        "sort_on": "sortable_title",
+        "b_start:": "int:"+$("input[name=b_start]").first().val(),
+    };
+    $.get(window.location.href, $.extend(defaults, overrides), function(data) {
+        var table_collection = $(data).find('#table-collection');
+        $("#table-collection").replaceWith(table_collection);
+    });
+}
+
+$.fn.enableTableCollectionReload = function(selector, overrides, prevent) {
+    var $target = this;
+    if(arguments.count < 3)
+        prevent = false;
+
+    $target.delegate(selector, "click", function(event) {
+        if(prevent)
+            event.preventDefault();
+        replaceCollectionTable(overrides);
+    });
+    return $target;
+};
+
+
 $(document).ready(function() {
 
+    if ($("body").hasClass('template-detailed_tabular_view')) {
+        var ccore = $("#content-core");
+        ccore.enableTableCollectionReload("#author-column", { "sort_on": "library_author", "ajax_load": true});
+        ccore.enableTableCollectionReload("#title-column", { "sort_on": "sortable_title", "ajax_load": true});
+        ccore.enableTableCollectionReload("#year-column", { "sort_on": "titleAuthorImprintCollation_imprint_year", "ajax_load": true});
+    }
+    
     if ($("body").hasClass('template-advancedsearch')) {
         $("#advanced_search_form").submit(function() {
             $('input').each(function() {
